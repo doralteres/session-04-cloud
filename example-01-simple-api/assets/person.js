78456cea37c4253bd65c23ececc90599171ab42a
Dor Alteresku <doralteres@gmail.com>
2024-01-02 19:56:23 +0200

    change name

--- example-01-simple-api/assets/person.js
+++ example-01-simple-api/assets/person.js
@@ -1,5 +1,5 @@
 const person = {
-  name: "Yossi Alteresku",
+  name: "Dor Alteresku",
   money: 1220,
   imageUrl:
     "https://media.licdn.com/dms/image/D4D03AQHrGjQT-Dh0PQ/profile-displayphoto-shrink_800_800/0/1677166454861?e=1709769600&v=beta&t=qsQ2mBfC6OWiy2jnYNM8S8Q1IxsAL--HbwV-0FsnJZM",
