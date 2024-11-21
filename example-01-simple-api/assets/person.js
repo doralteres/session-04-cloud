84c4d507bfd2a8fbf9155af65d3face56bbac270
Dor Alteresku <doralteres@gmail.com>
2024-11-21 19:50:55 +0200

    resize dors budget

--- example-01-simple-api/assets/person.js
+++ example-01-simple-api/assets/person.js
@@ -1,6 +1,6 @@
 const person = {
   name: "Dor Alteresku",
-  money: 1220,
+  money: 1250,
   imageUrl:
     "https://media.licdn.com/dms/image/v2/D4D03AQHrGjQT-Dh0PQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1677166454861?e=1737590400&v=beta&t=gqeDg23NdfW3Ddq2Mx-jpTfyrk0R6RNW-DP2f4hu5rI",
 };
